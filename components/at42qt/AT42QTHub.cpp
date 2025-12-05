defc92d93beddb0622f68b15ff122fa317c2a84c
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-12-06 00:53:21 +0100

    new oversample attempt

--- components/at42qt/AT42QTHub.cpp
+++ components/at42qt/AT42QTHub.cpp
@@ -12,9 +12,13 @@ void AT42QTHub::setup(){
     uint8_t nzv=1;
     this->write_register((uint8_t)RESET, &nzv, 1);
     
-    uint8_t low_thresh = 2; //default was 10, this is very low
+    uint8_t thresh = 2; //default was 10, this is very low
     for (uint8_t r = (uint8_t)KEY_DETECT_THRESHOLD; r<(uint8_t)KEY_CONTROL; r++)
-        this->write_register(r, &low_thresh, 1);
+        this->write_register(r, &thresh, 1);
+
+    uint8_t oversample = 0x50; //oversample=4^5 scale=2^0
+    for (uint8_t r = (uint8_t)KEY_DETECT_THRESHOLD; r<(uint8_t)KEY_CONTROL; r++)
+        this->write_register(r, &oversample, 1);
 
     uint8_t charge_time = 128;
     this->write_register((uint8_t)CHARGE_DURATION, &charge_time, 1);
