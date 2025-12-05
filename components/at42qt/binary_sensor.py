55a49f4c98301753787465049207a97ffa3a23e8
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-12-05 20:07:57 +0100

    channel

--- components/at42qt/binary_sensor.py
+++ components/at42qt/binary_sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_CHANNEL
 from . import AT42QTHub, CONF_AT42QT_HUB_ID
 
 DEPENDENCIES = ["at42qt"]
@@ -11,6 +11,7 @@ CONFIG_SCHEMA = (
     .extend(
         {
             cv.GenerateID(CONF_AT42QT_HUB_ID): cv.use_id(AT42QTHub),
+            cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=11),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -20,5 +21,6 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     paren = await cg.get_variable(config[CONF_AT42QT_HUB_ID])
     var = cg.new_Pvariable(config[CONF_ID])
+    cg.add(var.set_channel(config[CONF_CHANNEL]))
     await binary_sensor.register_binary_sensor(var, config)
     cg.add(paren.register_binary_sensor(var))
