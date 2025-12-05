9f61a65add87dee35e3c1a7ae83c9e22914999b4
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-12-05 20:53:20 +0100

    try making codegen use AT42QTChannel

--- components/at42qt/binary_sensor.py
+++ components/at42qt/binary_sensor.py
@@ -7,8 +7,7 @@ from . import AT42QTHub, CONF_AT42QT_HUB_ID
 DEPENDENCIES = ["at42qt"]
 
 CONFIG_SCHEMA = (
-    binary_sensor.binary_sensor_schema()
-    .extend(
+    binary_sensor.binary_sensor_schema(AT42QTChannel).extend(
         {
             cv.GenerateID(CONF_AT42QT_HUB_ID): cv.use_id(AT42QTHub),
             cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=11),
@@ -19,8 +18,7 @@ CONFIG_SCHEMA = (
 
 
 async def to_code(config):
-    paren = await cg.get_variable(config[CONF_AT42QT_HUB_ID])
-    var = cg.new_Pvariable(config[CONF_ID])
+    hub = await cg.get_variable(config[CONF_AT42QT_HUB_ID])
+    var = await binary_sensor.new_binary_sensor(config)
     cg.add(var.set_channel(config[CONF_CHANNEL]))
-    await binary_sensor.register_binary_sensor(var, config)
-    cg.add(paren.register_channel(var))
+    cg.add(hub.register_channel(var))
