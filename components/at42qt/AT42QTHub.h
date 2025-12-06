5d69b0d2ae3f74820a5b3e5d1b010ccd6d0e6c37
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-12-06 19:15:44 +0100

    oops

--- components/at42qt/AT42QTHub.h
+++ components/at42qt/AT42QTHub.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/sensor/sensor.h"
 
 
 namespace esphome {
