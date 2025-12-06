a69c24331deed224612e54d895849531c6894573
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-12-06 19:25:32 +0100

    oops

--- components/at42qt/sensor.py
+++ components/at42qt/sensor.py
@@ -38,7 +38,7 @@ CONFIG_SCHEMA = cv.Schema(
 ).extend(cv.polling_component_schema("10s"))
 
 async def to_code(config):    
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID], config[CONF_CHANNEL])
     await cg.register_component(var, config)
 
     if CONF_SENSOR_SIGNAL in config:
